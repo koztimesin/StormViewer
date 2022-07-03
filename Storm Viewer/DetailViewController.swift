83d19ac3f74d3f4d88a18e34a5bcf488e62cfa1e
koztimesin <a.isumairu01@gmail.com>
2022-07-03 17:12:29 +0300

    function to share image

--- Storm Viewer/DetailViewController.swift
+++ Storm Viewer/DetailViewController.swift
@@ -39,14 +39,14 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func shareTapped () {
+        guard let image = imageView.image?.jpegData(compressionQuality: 1) else {
+            print ("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
 
 }
